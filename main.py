99f916a23da46d48d062bedc1dc76bc0b7169c07
artem <tyrin.alex2007@gmail.com>
2023-01-25 07:35:06 +0300

    Написал вспомогательные функции

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import cv2
 import math as m
 import mediapipe as mp
+
+# Функция поиска расстояния между двумя точками по их координатам
+
+def findDistance(x1, y1, x2, y2):
+    dist = m.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return dist
+
+
+# Функция поиска угла
+
+def findAngle(x1, y1, x2, y2):
+    theta = m.acos((y2 - y1) * (-y1) / (m.sqrt(
+        (x2 - x1) ** 2 + (y2 - y1) ** 2) * y1))
+    degree = int(180 / m.pi) * theta
+    return degree
+
+
